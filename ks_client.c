c99252fdc206650c4e677e6deff0998799a47ea2
Renz Tolentino <renzjordan1@gmail.com>
2023-10-13 16:21:39 -0400

    adding comments

--- ks_client.c
+++ ks_client.c
@@ -11,6 +11,7 @@
 #define MAXLINESIZE 1024
 #define MAXOUTSIZE 2048
 
+//message struct
 struct message_s {
   long type;
   char keyword[MAXKEYWORD];
@@ -22,29 +23,35 @@ struct message_s {
 
 int main(int argc, char *argv[]) {
 
+    //keyword, dirpath as arguments
     char *keyword = argv[1];
     char *dirpath= argv[2];
 
+    //create message queue
     struct message_s message;
     int message_queue_id;
     key_t key;
 
+    //generate key to send messages between client and server
     if ((key = ftok("ks_server.c", 1)) == -1) {
         perror("ftok");
         exit(1);
     }
 
+    //Use the key to enter queue
     if ((message_queue_id = msgget(key, 0644)) == -1) {
         perror("msgget");
         exit(1);
     }
 
+    //Initialize message struct details
     message.type = 1;
     strcpy(message.keyword, keyword);
     strcpy(message.dirpath, dirpath);
 
     // printf("%s", message.content);
 
+    //Send message to queue
     if(msgsnd(message_queue_id, &message, MAXKEYWORD + MAXDIRPATH, 0) == -1) {
         perror("Error in msgsnd");
     }
--- ks_server.c
+++ ks_server.c
@@ -21,84 +21,108 @@
 #define MAXLINESIZE 1024
 #define MAXOUTSIZE 2048
 
+//message struct
 struct message_s {
   long type;
   char keyword[MAXKEYWORD];
   char dirpath[MAXDIRPATH];
 };
 
-//PLACEHOLDER CODE TO READ FILES
+//READ FILES
 void readFile(char *File, char *keyword){
 
     //Read in file input
     FILE* ptr = fopen(File, "r");
+
+    //Variables to read line by line
     char *line = malloc(MAXLINESIZE);
     size_t len = 0;
     ssize_t read;
 
+    //Validate file
     if (ptr == NULL) {
         printf("no such file.");
         exit(1);
     }
 
-
+    //variables to search through line  
     char *words = malloc(MAXLINESIZE);
     char *search = malloc(MAXLINESIZE);
 
+    //read line by line
     while ((read = getline(&line, &len, ptr)) != -1) {
         // printf("%s\n", line);
+
+        //tokenize a line (delimited by " ")
         strcpy(search, line);
         words = strtok(search, " ");
+
+        //get next word token until none left
         while( words != NULL ) {
+          //remove new lines
           words[strcspn(words, "\n")] = '\0';
+
           // printf("%s %d\n", words, strcmp(words, keyword));
-            if(strcmp(words, keyword) == 0){
-                printf("%s:%s", keyword, line);
-                // printf("%s\n", line);
-                break;
-            }
-            words = strtok(NULL, " ");
+          
+          //if word token matches keyword, print line
+          if(strcmp(words, keyword) == 0){
+              printf("%s:%s", keyword, line);
+              // printf("%s\n", line);
+              break;
+          }
+
+          //get next word token
+          words = strtok(NULL, " ");
         }
         
     }
-
-
     printf("\n");
-    fclose(ptr);
 
+    //close and free
+    fclose(ptr);
     free(line);
 
 
 }
 
+//READ DIRECTORY
 void readFolder(char *dirpath, char *keyword){
-  struct dirent *de;  // Pointer for directory entry 
+  //directory entry 
+  struct dirent *de;  
 
-  // opendir() returns a pointer of DIR type.  
+  //directory 
   DIR *dir = opendir(dirpath); 
+
+  //track viable files to open
   int count = 0;
 
-  if (dir == NULL)  // opendir returns NULL if couldn't open directory 
-  { 
+  //Validate directory
+  if (dir == NULL){
       printf("Error opening directory" ); 
       return; 
   } 
 
-  // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
-  // for readdir() 
+  
   char *File = malloc(MAXDIRPATH);
+
+  //readdir gets next directory entry
   while ((de = readdir(dir)) != NULL){
+    //Don't include parent paths
     if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) {
         continue;
     }
     count++;
     // printf("%s\n", de->d_name); 
+
+    //Get full file path
     sprintf(File, "%s%s", dirpath, de->d_name);
     printf("%s\n", File);
     readFile(File, keyword);
 
   }
   // printf("%d\n", count);
+
+  //close and free
   closedir(dir);     
 } 
 
@@ -117,23 +141,30 @@ int main(void) {
   pthread_t tid; //thread itendifier
   pthread_attr_t attr; //thread attributes
 
-  // printf("what");
+  //generate key to send messages between client and server
   if ((key = ftok("ks_server.c", 1)) == -1) {
     perror("ftok");
     exit(1);
   }
 
+  //Use the key to enter queue
   if ((message_queue_id = msgget(key, 0644 | IPC_CREAT)) == -1) {
     perror("msgget");
     exit(1);
   }
 
+  //Loop forever
   for(;;) {
+
+    //Receive message from queue
     if (msgrcv(message_queue_id, &message, MAXKEYWORD + MAXDIRPATH, 0, 0) == -1) {
       perror("msgrcv");
       exit(1);
     }
+    
     printf("%s %s\n\n", message.keyword, message.dirpath);
+    
+    //Read the folder passed in message
     readFolder(message.dirpath, message.keyword);
   }
 
